1ee40ecfbb2802c3fe5da950bc8e53f5adb1cd5f
Emilia Garipova <emilia270627@gmail.com>
2020-04-03 21:32:08 +0300

    Added put & delete todos routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,44 +1,36 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const ToDo = require('../models/ToDo')
-const auth = require('../middleware/auth')
+const ToDo = require("../models/ToDo")
+const auth = require("../middleware/auth")
 
-require('dotenv').config({ path: 'var.env' });
-
-router.get('', auth, async (req, res) => {
+router.get("", auth, async (req, res) => {
     try {
         let todos = await ToDo.find({ owner: req.user.userId });
         res.json(todos);
     } catch (e) {
-        res.status(500).json({ message: "Something went wrong" })
+        res.status(500).json({ message: "Something went wrong" });
     }
 });
 
-router.post('', auth, async (req, res) => {
+router.post("", auth, async (req, res) => {
     let date = Date.now();
     let status = "new";
     let text = req.body.text;
     let todo = new ToDo({
-        text, status, date, owner: req.user.id
-    })
-    await todo.save()
-    res.status(200).json({ todo })
+        text, status, date, owner: req.user.userId
+    });
+    await todo.save();
+    res.status(200).json({ todo });
 });
 
+router.delete("/:id", auth, async (req, res) => {
+    await ToDo.deleteOne({ _id: req.params.id });
+    res.status(200);
+});
 
-// app.delete("/todos/:id", async (req, res) => {
-//     var user = await users.reload(users.authenticate(req));
-//     let id = req.params.id;
-//     delete user.todos[id];
-//     users.updateUser(user);
-// });
-
-// app.put("/todos/:id", async (req, res) => {
-//     var user = await users.reload(users.authenticate(req));
-//     let id = req.params.id;
-//     let newToDo = user.todos[id];
-//     newToDo.status = req.body.status;
-//     users.updateToDo(user, user.todos[id]);
-// })
+router.put("/:id", auth, async (req, res) => {
+    await ToDo.updateOne({ _id: req.params.id }, { status: req.body.status});
+    res.status(200);
+});
 
 module.exports = router;

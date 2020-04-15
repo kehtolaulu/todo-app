2815f355bb1e35b5e5761a5eaa2f00ff06d01b6d
Emilia Garipova <emilia270627@gmail.com>
2020-04-15 22:28:28 +0300

    Added todo insert to list

--- routes/toDoLists.js
+++ routes/toDoLists.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const User = require("../models/User");
 const ToDoList = require("../models/ToDoList");
+const ToDo = require("../models/ToDo");
 const auth = require("../middleware/auth");
 
 const router = Router();
@@ -19,6 +20,24 @@ router.get("/:id/todos", auth, async (req, res) => {
     try {
         let toDoList = await ToDoList.findOne({ _id: req.params.id });
         res.json(toDoList.toDos);
+    } catch (e) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+});
+
+router.post("/:id/todos", auth, async (req, res) => {
+    try {
+        let toDoList = await ToDoList.findOne({ _id: req.params.id });
+        let date = Date.now();
+        let status = "new";
+        let text = req.body.text;
+        let todo = new ToDo({
+            text, status, date, owner: req.user.userId
+        });
+        await todo.save();
+        toDoList.toDos.push(todo);
+        toDoList.save();
+        res.status(200).json({ todo });
     } catch (e) {
         res.status(500).json({ message: "Something went wrong" });
     }
